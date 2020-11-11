4164026e0566675acdfe15ab2c72f0e6f4d941bf
dwd0497 <dwd0497@gmail.com>
2020-11-11 14:25:08 +0500

    Добавил возможность добавления нескольких фотографий

--- js/imagePreview.js
+++ js/imagePreview.js
@@ -3,8 +3,6 @@ const IMG_TAG_NAME = `IMG`;
 
 const createAndRenderImgElement = (parentElement) => {
   const imgElement = document.createElement(`img`);
-  imgElement.style.width = `44px`;
-  imgElement.style.height = `40px`;
   parentElement.appendChild(imgElement);
   return imgElement;
 };
@@ -12,24 +10,26 @@ const createAndRenderImgElement = (parentElement) => {
 export const getImagePreview = (input, preview, type) => {
   const method = type ? `addEventListener` : `removeEventListener`;
   input[method](`change`, () => {
-    const file = input.files[0];
-    const fileName = file.name.toLowerCase();
+    Array.from(input.files).forEach((file) => {
+      const fileName = file.name.toLowerCase();
 
-    const matches = FILE_TYPES.some((fileType) => {
-      return fileName.endsWith(fileType);
+      const matches = FILE_TYPES.some((fileType) => {
+        return fileName.endsWith(fileType);
+      });
+
+      if (matches) {
+        const reader = new FileReader();
+
+        reader.addEventListener(`load`, () => {
+          if (preview.tagName !== IMG_TAG_NAME) {
+            createAndRenderImgElement(preview).src = reader.result;
+          } else {
+            preview.src = reader.result;
+          }
+        });
+        reader.readAsDataURL(file);
+      }
     });
 
-    if (matches) {
-      const reader = new FileReader();
-
-      reader.addEventListener(`load`, () => {
-        if (preview.tagName !== IMG_TAG_NAME) {
-          createAndRenderImgElement(preview).src = reader.result;
-        } else {
-          preview.src = reader.result;
-        }
-      });
-      reader.readAsDataURL(file);
-    }
   });
 };
