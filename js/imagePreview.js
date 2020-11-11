e1480546f4ba0e1b09021956774db799451eebfb
dwd0497 <dwd0497@gmail.com>
2020-11-12 01:20:44 +0500

    Изменил функцию показа добавленных изображений

--- js/imagePreview.js
+++ js/imagePreview.js
@@ -3,15 +3,15 @@ import {forEach} from "./util.js";
 const FILE_TYPES = [`gif`, `jpg`, `jpeg`, `png`];
 const IMG_TAG_NAME = `IMG`;
 
-const createAndRenderImgElement = (parentElement) => {
-  const imgElement = document.createElement(`img`);
-  parentElement.appendChild(imgElement);
-  return imgElement;
+const getImgElement = () => {
+  return document.createElement(`img`);
 };
 
 export const getImagePreview = (input, preview, type) => {
   const method = type ? `addEventListener` : `removeEventListener`;
   input[method](`change`, () => {
+    const fragment = document.createDocumentFragment();
+    let filesCounter = 0;
     forEach(input.files, (file) => {
       const fileName = file.name.toLowerCase();
 
@@ -24,7 +24,13 @@ export const getImagePreview = (input, preview, type) => {
 
         reader.addEventListener(`load`, () => {
           if (preview.tagName !== IMG_TAG_NAME) {
-            createAndRenderImgElement(preview).src = reader.result;
+            const imgElement = getImgElement();
+            imgElement.src = reader.result;
+            fragment.appendChild(imgElement);
+            filesCounter++;
+            if (filesCounter === input.files.length) {
+              preview.appendChild(fragment);
+            }
           } else {
             preview.src = reader.result;
           }
